ef1637e4ff1564cf78f212588641a58c61f07a58
Rafael Morais <rafael.henriquemorais@gmail.com>
2022-05-21 18:22:34 -0300

    main - first commit

---
+++ teste-azure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace teste_azure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
